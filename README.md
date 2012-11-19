539caddc4a1e87e6bbb28e669ecfb365dda347e1
Brett Koonce <koonce@gmail.com>
2012-11-18 22:20:00 -0600

    homebrew-hydrometer
    this is a basic script to help update homebrew packages.
    released under the bsd license.

---
+++ README.md
@@ -0,0 +1,72 @@
+
+Hydrometer
+==
+
+Want to submit package updates to homebrew?
+
+Don't want to find packages that need updating by hand?
+
+This is a script to help automate the review process!
+
+
+Installation
+===
+
+This is python3 code.  Install python3 (and pip3) from brew like so:
+
+	brew install python3
+
+Now you can install BeautifulSoup, a python3 html parser the scripts rely on:
+
+	pip3 install beautifulsoup4
+
+Goto the terminal type 'python3' to start a new shell.  Then type 'import bs4'.
+
+If this looks like what you see (no angry messages about errors), you're good to go!
+
+	$ python3
+	Python 3.3.0 (default, Nov  3 2012, 09:45:08) 
+	[GCC 4.2.1 Compatible Apple Clang 4.1 ((tags/Apple/clang-421.11.66))] on darwin
+	Type "help", "copyright", "credits" or "license" for more information.
+	>>> import bs4
+	>>> 
+
+
+Basic usage
+===
+
+Currently hydrometer supports two options:
+
+	-s, --sourceforge:  this will scan for sourceforge updates in homebrew (~500 packages)
+	-g, --googlecode:  this will scan for googlecode updates in homebrew (~160 packages)
+
+You're more than welcome to run the script interactively, but you'll probably want something like:
+
+	python3 hydrometer -s > sourceforge_updates.txt
+
+The end result will be a file that contains:
+
+	1) a line from sourceforge packages
+	2) hydrometer's best guess for the cooresponding package's latest version/downloads
+	3) some spacing to keep things sane
+
+Now, you just need to look through sourceforge_updates.txt for packages that need updating.
+
+
+Final step
+===
+
+Update the package.  Submit a patch.  Have a beer to celebrate. :3
+
+
+Future improvements
+===
+
+The googlecode parser is a bit garish, I know.
+
+The other two good targets for updates are:
+
+	gnu/savannah (~220 packages)
+	github (~220 packages)
+
+
---
+++ hydrometer.py
@@ -0,0 +1,136 @@
+#!/usr/local/bin/python3
+
+from bs4 import BeautifulSoup
+
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+
+import sys, string
+import re
+
+from subprocess import Popen, PIPE
+
+import subprocess
+import argparse
+
+def sourceforge_parse(line):
+    
+    m = re.search('(?<=net/)\w+', line)
+
+    if m:
+        
+        name = ''
+        
+        if m.group(0)=='project' or m.group(0)=="projects":
+            m = re.search('(?<='+m.group(0)+'/)\w+', line)
+            if m:
+                name = m.group(0)
+                #print (m.group(0), line)
+        else:
+            name = m.group(0)
+            #print (m.group(0), line)
+            
+        if name:
+            
+            url = 'http://sourceforge.net/projects/' + name + '/files/'
+        
+            print (url)
+            
+
+            req = Request(url)
+            try:
+                response = urlopen(req)
+            except HTTPError as e:
+                print('The server couldn\'t fulfill the request.')
+                print('Error code: ', e.code)
+            except URLError as e:
+                print('We failed to reach a server.')
+                print('Reason: ', e.reason)
+            
+            else:            
+                data = response.read()
+            
+                if data:
+                    soup = BeautifulSoup(data)
+            
+                    for dl in soup.find_all("div", class_="download-bar"):
+                        a = dl.contents[1]
+                    
+                        print (line)
+                        print ('                        ',a.contents[1].contents[1])
+                        print ('\n\n')
+                        
+                        
+                        
+                        
+def googlecode_parse(line):
+
+     m = re.search('(?<=//)\w+', line)
+    
+     if m:
+
+         url = 'http://code.google.com/p/' + m.group(0) + '/downloads/list'
+        
+         req = Request(url)
+         try:
+             response = urlopen(req)
+         except HTTPError as e:
+             print('The server couldn\'t fulfill the request.')
+             print('Error code: ', e.code)
+         except URLError as e:
+             print('We failed to reach a server.')
+             print('Reason: ', e.reason)
+            
+         else:            
+             data = response.read()
+            
+             if data:
+                 soup = BeautifulSoup(data)
+                
+                 print ('\n\n\n\n')
+                 print (line)
+                
+                 for dl in soup.find_all("tr",class_='ifOpened'):
+                    
+                     print (dl)
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Please specify which source of homebrew packages you wish to parse.')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-s", "--sourceforge", action="store_true")
+    group.add_argument("-g", "--googlecode", action="store_true")
+
+    args = parser.parse_args()
+        
+    if args.sourceforge:
+        print ('Building a list of files to process, this may take some time...')    
+        process = subprocess.Popen("grep url /usr/local/Library/Formula/*.rb | grep sourceforge",
+                                     shell=True,
+                                     stdout=subprocess.PIPE )
+        stdout_data = str( process.communicate()[0], encoding='utf8' )
+        
+        package_list = stdout_data.split('\n')
+    
+        for line in package_list:
+            sourceforge_parse(line)
+        
+    elif args.googlecode:
+        print ('Building a list of files to process, this may take some time...')    
+        process = subprocess.Popen("grep url /usr/local/Library/Formula/*.rb | grep googlecode",
+                                     shell=True,
+                                     stdout=subprocess.PIPE )
+        stdout_data = str( process.communicate()[0], encoding='utf8' )
+        
+        package_list = stdout_data.split('\n')
+    
+        for line in package_list:
+            googlecode_parse(line)
+
+    else:
+        print ('No package source specified, halting.  (try --help)')
+    
+    
+    
